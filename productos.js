8440ffa9bdd3bf7afab148a13cc81a3a3fecc438
Fedesca001 <familadore@gmail.com>
2021-10-21 04:23:38 -0300

    Carga de img

--- productos.js
+++ productos.js
@@ -1,193 +1,347 @@
 
 const beb = [
     {
-        "img": "cocagrande.jpg",
+        "img": "images/cocagrande.jpg",
         "nombre": "Coca Cola",
         "descp": "Grande",
         "precio": "$200",
         "id": 01
     },
     {
-        "img": "img/",
-        "nombre": "",
-        "descp": "",
-        "precio": "$",
+        "img": "images/cocazero.jpg",
+        "nombre": "Coca zero",
+        "descp": "s/azucar",
+        "precio": "$199",
         "id": 02
     },
     {
-        "img": "img/",
-        "nombre": "",
-        "descp": "",
-        "precio": "$",
+        "img": "images/cocamedio.jpg",
+        "nombre": "Coca cola chica",
+        "descp": "500ml",
+        "precio": "$90",
         "id": 03
     },
     {
-        "img": "img/",
-        "nombre": "",
-        "descp": "",
-        "precio": "$",
+        "img": "images/cocalata",
+        "nombre": "Lata de coca cola",
+        "descp": "lata fria",
+        "precio": "$85",
         "id": 04
     },
     {
-        "img": "img/",
-        "nombre": "",
-        "descp": "",
-        "precio": "$",
+        "img": "images/pepsi2L.jpg",
+        "nombre": "Pepsi",
+        "descp": "Botella 2L",
+        "precio": "$200",
         "id": 05
+    },
+    {
+        "img": "images/pepsimedioL.jpg",
+        "nombre": "Pepsi",
+        "descp": "Botella 1/2L",
+        "precio": "$120",
+        "id": 06
+    },
+    {
+        "img": "images/pepsilata.jpg",
+        "nombre": "Pepsi",
+        "descp": "Lata",
+        "precio": "$80",
+        "id": 07
+    },
+    {
+        "img": "images/spritegrande.jpg",
+        "nombre": "Sprite 2L",
+        "descp": "Botella 2L",
+        "precio": "$200",
+        "id": 08
+    },
+    {
+        "img": "images/spritemedio.jpg",
+        "nombre": "Sprite 1/2L",
+        "descp": "Botella 1/2L",
+        "precio": "$120",
+        "id": 09
+    },
+    {
+        "img": "images/Sprite-lata.jpg",
+        "nombre": "Sprite",
+        "descp": "lata",
+        "precio": "$80",
+        "id": 010
+    },
+    {
+        "img": "images/baggiocaja.jpg",
+        "nombre": "Baggio grande",
+        "descp": "Caja",
+        "precio": "$200",
+        "id": 011
+    },
+    {
+        "img": "images/baggiochico.jpg",
+        "nombre": "Baggio chico",
+        "descp": "Cajita",
+        "precio": "$200",
+        "id": 012
+    },
+    {
+        "img": "images/cepita.jpg",
+        "nombre": "Cepita",
+        "descp": "Botella 1,5L",
+        "precio": "$210",
+        "id": 013
+    },
+    {
+        "img": "images/cepitacaja.jpg",
+        "nombre": "Cepita",
+        "descp": "Cajita",
+        "precio": "$70",
+        "id": 014
+    },
+    {
+        "img": "images/cepitachica.jpg",
+        "nombre": "Cepita",
+        "descp": "Botella 1/2L",
+        "precio": "$90",
+        "id": 015
     },
 ];
 
 const caram = [
     {
-        "img": "img/",
-        "nombre": "",
-        "descp": "",
-        "precio": "$",
+        "img": "images/caramasticable.jpg",
+        "nombre": "Masticables",
+        "descp": "Caramelos masticables",
+        "precio": "$20",
         "id": 11
     },
     {
-        "img": "img/",
-        "nombre": "",
-        "descp": "",
-        "precio": "$",
+        "img": "images/caramgomi.jpg",
+        "nombre": "Gomitas",
+        "descp": "Caramelos Gomita",
+        "precio": "$24",
         "id": 12
     },
     {
-        "img": "img/",
-        "nombre": "",
-        "descp": "",
-        "precio": "$",
+        "img": "images/caramiel.png",
+        "nombre": "Caramelos de Miel",
+        "descp": "Bolsa de 200u",
+        "precio": "$160",
         "id": 13
+    },
+    {
+        "img": "images/fizztiras.jpg",
+        "nombre": "Caramelos Fizz",
+        "descp": "Tiras de sabores",
+        "precio": "$60",
+        "id": 14
+    },
+    {
+        "img": "images/paleta.jpg",
+        "nombre": "Paleta",
+        "descp": "Paleta de caramelo",
+        "precio": "$80",
+        "id": 15
+    },
+    {
+        "img": "images/picodulcebolsa.jpg",
+        "nombre": "Caramelos Pico Dulce",
+        "descp": "Bolsa de caramelos",
+        "precio": "$160",
+        "id": 16
+    },
+    {
+        "img": "images/suguscaja.jpg",
+        "nombre": "Sugus",
+        "descp": "Caja de caramelos",
+        "precio": "$50",
+        "id": 17
+    },
+    {
+        "img": "images/moritas.jpg",
+        "nombre": "Moritas",
+        "descp": "Venta al peso",
+        "precio": "$30",
+        "id": 17
     },
 ];
 
 const almac = [
     {
-        "img": "img/",
-        "nombre": "",
-        "descp": "",
-        "precio": "$",
+        "img": "images/fideo.jpg",
+        "nombre": "Fideos",
+        "descp": "Knor",
+        "precio": "$70",
         "id": 21
     },
     {
-        "img": "img/",
-        "nombre": "",
-        "descp": "",
-        "precio": "$",
+        "img": "images/lechepolvo.jpeg",
+        "nombre": "Leche en polvo",
+        "descp": "Caja descremada",
+        "precio": "$250",
         "id": 22
+    },
+    {
+        "img": "images/mermeladaarcor.jpg",
+        "nombre": "Mermelada",
+        "descp": "Sabor durazno",
+        "precio": "$130",
+        "id": 23
+    },
+    {
+        "img": "images/mocovi.jpg",
+        "nombre": "Arroz en caja",
+        "descp": "Mocovi",
+        "precio": "$80",
+        "id": 24
+    },
+    {
+        "img": "images/arvejas.jpg",
+        "nombre": "Arvejas",
+        "descp": "Lata",
+        "precio": "$85",
+        "id": 25
+    },
+    {
+        "img": "images/natura.jpg",
+        "nombre": "Aceite Natura",
+        "descp": "2L",
+        "precio": "$290",
+        "id": 26
     },
 ];
 
 const chocolts = [
     {
-        "img": "img/",
-        "nombre": "",
-        "descp": "",
-        "precio": "$",
-        "id": 31
+        "img": "images/tofinegro.jpg",
+        "nombre": "Tofi",
+        "descp": "Negro",
+        "precio": "$40",
+        "id": 41
     },
     {
-        "img": "img/",
-        "nombre": "",
-        "descp": "",
-        "precio": "$",
-        "id": 32
+        "img": "images/tofiblanco.png",
+        "nombre": "Tofi",
+        "descp": "Blanco",
+        "precio": "$40",
+        "id": 42
     },
     {
-        "img": "img/",
-        "nombre": "",
-        "descp": "",
-        "precio": "$",
-        "id": 33
+        "img": "images/milka.jpg",
+        "nombre": "Milka",
+        "descp": "60g",
+        "precio": "$90",
+        "id": 43
+    },
+    {
+        "img": "images/cacao.png",
+        "nombre": "Chocolate arcor",
+        "descp": "Barra 70g",
+        "precio": "$70",
+        "id": 44
+    },
+    {
+        "img": "images/aguila.jpg",
+        "nombre": "Chocolate aguila",
+        "descp": "Barra 70g",
+        "precio": "$150",
+        "id": 45
     },
 ];
 
 const farm = [
     {
-        "img": "img/",
-        "nombre": "",
-        "descp": "",
-        "precio": "$",
-        "id": 41
+        "img": "images/ibuprofeno.png",
+        "nombre": "Ibuprofeno",
+        "descp": "Ibu 600mg",
+        "precio": "$45",
+        "id": 31
     },
     {
-        "img": "img/",
-        "nombre": "",
-        "descp": "",
-        "precio": "$",
-        "id": 42
+        "img": "images/actron.png",
+        "nombre": "Actron",
+        "descp": "Caja de 12u",
+        "precio": "$45",
+        "id": 32
     },
     {
-        "img": "img/",
-        "nombre": "",
-        "descp": "",
-        "precio": "$",
-        "id": 43
-    },
-    {
-        "img": "img/",
-        "nombre": "",
-        "descp": "",
-        "precio": "$",
-        "id": 44
+        "img": "images/curitas.jpg",
+        "nombre": "Curitas",
+        "descp": "Banditas",
+        "precio": "$20",
+        "id": 33
     },
 ];
 
 const cerv = [
     {
-        "img": "img/",
-        "nombre": "",
-        "descp": "",
-        "precio": "$",
+        "img": "images/budbotella.jpg",
+        "nombre": "Budweiser botella",
+        "descp": "botella 1.5L",
+        "precio": "$140",
         "id": 51
     },
     {
-        "img": "img/",
-        "nombre": "",
-        "descp": "",
-        "precio": "$",
+        "img": "images/latabud.png",
+        "nombre": "Budweiser lata",
+        "descp": "Lata 3.75 ml",
+        "precio": "$100",
         "id": 52
     },
     {
-        "img": "img/",
-        "nombre": "",
-        "descp": "",
-        "precio": "$",
+        "img": "images/andesbotella.jpg",
+        "nombre": "Andes",
+        "descp": "botella 1L",
+        "precio": "$130",
         "id": 53
     },
     {
-        "img": "img/",
-        "nombre": "",
-        "descp": "",
-        "precio": "$",
+        "img": "images/andeslata.jfif",
+        "nombre": "Andes lata",
+        "descp": "Lata",
+        "precio": "$95",
+        "id": 54
+    },
+    {
+        "img": "images/botellaquilmes.jpeg",
+        "nombre": "Quilmes botella",
+        "descp": "Botella 1L",
+        "precio": "$125",
+        "id": 54
+    },
+    {
+        "img": "images/lataquilmes.jpg",
+        "nombre": "Quilmes lata",
+        "descp": "Lata 300ml",
+        "precio": "$95",
         "id": 54
     },
 ];
 
 const tabac = [
     {
-        "img": "img/",
+        "img": "images/",
         "nombre": "",
         "descp": "",
         "precio": "$",
         "id": 61
     },
     {
-        "img": "img/",
+        "img": "images/",
         "nombre": "",
         "descp": "",
         "precio": "$",
         "id": 62
     },
     {
-        "img": "img/",
+        "img": "images/",
         "nombre": "",
         "descp": "",
         "precio": "$",
         "id": 63
     },
     {
-        "img": "img/",
+        "img": "images/",
         "nombre": "",
         "descp": "",
         "precio": "$",
@@ -197,21 +351,21 @@ const tabac = [
 
 const gallets = [
     {
-        "img": "img/",
+        "img": "images/",
         "nombre": "",
         "descp": "",
         "precio": "$",
         "id": 71
     },
     {
-        "img": "img/",
+        "img": "images/",
         "nombre": "",
         "descp": "",
         "precio": "$",
         "id": 72
     },
     {
-        "img": "img/",
+        "img": "images/",
         "nombre": "",
         "descp": "",
         "precio": "$",
